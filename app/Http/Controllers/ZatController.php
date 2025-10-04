14b37693eebe19573e6295f643f00cc8d92d6bff
ahmed maher <ahmedmaher1792001@gmail.com>
2025-10-04 20:52:48 +0300

    last

--- app/Http/Controllers/ZatController.php
+++ app/Http/Controllers/ZatController.php
@@ -153,7 +153,7 @@ class ZatController extends Controller
             ])->withHeaders([
                 'Content-Type'   => 'application/json',
                 'Accept-Version' => '1.2.0',
-                'Authorization'  => 'Bearer ' . $binary_security_token,
+                'Authorization'  => 'Bearer ' . $binarySecurityToken2,
                     'Accept-Language: en',
             ])->post('https://gw-fatoora.zatca.gov.sa/e-invoicing/developer-portal/invoices/reporting/single', [
                 'invoiceHash' => $invoice_hash,
