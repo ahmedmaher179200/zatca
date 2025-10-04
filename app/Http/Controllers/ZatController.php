3caec4d762140bf2bedf6c34bbca38092fa5493e
ahmed maher <ahmedmaher1792001@gmail.com>
2025-10-04 19:01:42 +0300

    last

--- app/Http/Controllers/ZatController.php
+++ app/Http/Controllers/ZatController.php
@@ -84,8 +84,8 @@ class ZatController extends Controller
         
         // Issue a new compliance cert for the EGS
         list($request_id, $binary_security_token, $secret) = $egs->issueComplianceCertificate('123345', $csr);
-        echo 'username:' . $binary_security_token . "<br>";
-        echo 'password:' . $secret . "<br>";
+        echo 'binary_security_token:' . $binary_security_token . "<br>";
+        echo 'secret:' . $secret . "<br>";
 
         // Sign invoice
         list($signed_invoice_string, $invoice_hash, $qr,$public_key) = $egs->signInvoice($invoice, $egs_unit, $binary_security_token, $private_key);
