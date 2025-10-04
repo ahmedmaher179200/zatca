34627315a39aa8a54141fa75484b9d001fc89a6c
ahmed maher <ahmedmaher1792001@gmail.com>
2025-10-04 17:35:14 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/test', [ZatController::class, 'index']);
+Route::get('/', [ZatController::class, 'index']);
