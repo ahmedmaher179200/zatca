70c8e2ef69664f4cc6f6441b9bf502a7db99f67e
ahmed maher <ahmedmaher1792001@gmail.com>
2025-10-05 16:08:37 +0300

    last

--- app/Services/ZatcaServices/API.php
+++ app/Services/ZatcaServices/API.php
@@ -56,7 +56,7 @@ class API
             $response = json_decode($response);
             if ($http_code != 200) throw new Exception('Error issuing a compliance certificate.');
             $issued_certificate = base64_decode($response->binarySecurityToken);
-            $response->binarySecurityToken = $response->binarySecurityToken;
+            $response->binarySecurityToken = "-----BEGIN CERTIFICATE-----\n{$issued_certificate}\n-----END CERTIFICATE-----";
 
             return $response;
         };
