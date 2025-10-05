3f77a8c6a82efb5f71ad396f03f73fcce6d220ac
ahmed maher <ahmedmaher1792001@gmail.com>
2025-10-05 16:07:20 +0300

    last

--- app/Http/Controllers/ZatController.php
+++ app/Http/Controllers/ZatController.php
@@ -8,7 +8,6 @@ use Endroid\QrCode\Color\Color;
 use Endroid\QrCode\Writer\PngWriter;
 use Endroid\QrCode\Encoding\Encoding;
 use Illuminate\Support\Facades\Http;
-use Mohammad\Zatca\ZATCA\EGS as ZATCAEGS;
 
 class ZatController extends Controller
 {
@@ -64,7 +63,7 @@ class ZatController extends Controller
             $invoice['line_items'][] = $line_items[$i];
         }
 
-        $egs = new ZATCAEGS($egs_unit);
+        $egs = new EGS($egs_unit);
         $egs->production = false;
         list($private_key, $csr) = $egs->generateNewKeysAndCSR('Qr');
         //echo 'Private Key:' . $private_key . "<br>";
